20deaf90f5e429d18444ec6d3647eb5705ec3c08
Amine BENSALAH <aminebnsalah2020@gmail.com>
2024-04-15 14:10:02 +0100

    Amine BENSALAH/Partie1

---
+++ src/Controller/SongController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SongController extends AbstractController
+{
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'], name: 'api_songs_get_one')]
+    function getSong(int $id): Response
+    {
+
+        // TODO query the database
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+        return $this->json($song);
+    }
+}
