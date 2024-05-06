73cc3a0ab85b84882dd711df269a18d7f78e98ed
Amine BENSALAH <aminebnsalah2020@gmail.com>
2024-05-06 13:56:26 +0100

    Param Converter & 404's

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -36,11 +36,8 @@ class MixController extends AbstractController
     }
 
     #[Route('/mix/{id}', name: 'app_mix_show')]
-    public function show($id, VinylMixRepository $mixRepository): Response
+    public function show($id, VinylMix $mix): Response
     {
-        $mix = $mixRepository->find($id);
-        //dd($id);
-
         return $this->render('mix/show.html.twig', [
             'mix' => $mix,
         ]);
