51abe0ec52172fbba66479e8292852c65b219116
Amine BENSALAH <aminebnsalah2020@gmail.com>
2024-05-06 15:34:04 +0100

    Doctrine Extensions: Timestampable

--- src/Entity/VinylMix.php
+++ src/Entity/VinylMix.php
@@ -5,10 +5,12 @@ namespace App\Entity;
 use App\Repository\VinylMixRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: VinylMixRepository::class)]
 class VinylMix
 {
+    #[Gedmo\Timestampable()]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
